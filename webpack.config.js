20d1a3c4d16e58cad85028ce5f8f6345a8a75589
Sam Wheeler <swheeler111@gmail.com>
2017-03-20 11:30:41 -0400

    got webpack working

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,13 @@ const path = require('path');
 module.exports = {
   entry: './browser/index.jsx', // the starting point for our program
   output: {
-    path: path.join(__dirname, '/browser'), // absolute path for the directory where we want the output to be placed
-    filename: 'bundle.js',
+    path: path.join(__dirname, '/public'), // absolute path for the directory where we want the output to be placed
+    filename: './public/bundle.js',
+  },
+  context: __dirname,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
   },
   module: {
     loaders: [
