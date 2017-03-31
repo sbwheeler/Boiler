81200d8d17cc3a4b8e03e177b669e66b32c91084
Sam Wheeler <swheeler111@gmail.com>
2017-03-31 10:29:28 -0400

    commenting for clarity

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).end();
 });
 
+// this will default any unknown route to send them our index.html, preventing the CANNOT GET /route error
 app.get('/*', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
